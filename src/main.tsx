397d345d7afea44139e3e87ef5f2d2049d9785c6
VBoudreau55 <vancebou55@gmail.com>
2025-03-13 19:51:16 -0400

    cognito auth setup doesn't work

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./styles/index.css";
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+import { AuthProvider } from "react-oidc-context";
 
-Amplify.configure(outputs);
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_ZakuADvHR",
+  client_id: "78e46q6f25uu1gahoqbjilqt9c",
+  redirect_uri: "https://main.d1sdgkd4wd9p19.amplifyapp.com/",
+  response_type: "code",
+  scope: "email openid phone",
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
